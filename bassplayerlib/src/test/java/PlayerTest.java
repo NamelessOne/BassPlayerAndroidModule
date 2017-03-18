4d01069a18a992075c147cad350f7b25c5505c98
NamelessOne <nlessone@gmail.com>
2017-03-18 09:58:15 +0500

    travis ci test

---
+++ bassplayerlib/src/test/java/PlayerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by namelessone
+ * on 18.03.17.
+ */
+
+public class PlayerTest {
+    @Test
+    public void exampleTest() throws Exception {
+        Assert.assertEquals(2 * 2, 4);
+    }
+}
